eed9dff35b244054d6836d292c7d4b4a2f598689
zyh <617754841@qq.com>
2017-08-10 21:02:50 +0800

    首页基本完成

--- wx-sw-shake/pages/scan/index/index.js
+++ wx-sw-shake/pages/scan/index/index.js
@@ -1,12 +1,14 @@
 // index.js
 Page({
   data: {
+    isNight: false,
+    rule_show: false,
+    nowSwiper: 0,
+    progress: 0,
+    maps: [[0, 2, 3, 2, 1, 5, 6, 7, 8, 9], [0], [0]],
+    prize: [[10, false], [20, false], [30, false]],
     user: {
       img: '',
-      isNight: false,
-      nowSwiper: 0,
-      progress: 0,
-      maps: [],
     }
   },
   onLoad: function (options) {
@@ -26,8 +28,42 @@ Page({
       isNight: !this.data.isNight
     })
   },
-  showRule: function () { },
+  showRule: function () {
+    this.setData({
+      rule_show: true
+    })
+  },
+  closeRule: function () {
+    this.setData({
+      rule_show: false
+    })
+  },
   scan: function () { },
   share: function () { },
-  swiperChange: function () { },
+  swiperChange: function (e) {
+    this.setData({
+      nowSwiper: e.detail.current
+    })
+  },
+  swiperTo: function (e) {
+    var direction = parseInt(e.target.dataset.type, 10)
+    var i = this.data.nowSwiper + direction;
+    i = Math.max(0, Math.min(2, i));
+    this.setData({
+      nowSwiper: i
+    })
+  },
+  clickItem: function(e) {
+
+  },
+  clickBox: function(e) {
+    var isOpen = e.currentTarget.dataset.open;
+    var i = e.currentTarget.dataset.index;
+    if (!isOpen) {
+      this.setData({
+
+      })
+    }
+    console.log(isOpen, typeof isOpen)
+  },
 })
