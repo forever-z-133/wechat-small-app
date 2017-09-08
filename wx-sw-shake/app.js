8a05c105a4b2d0ca5a67523bcab9d414a0bd5a52
zyh <617754841@qq.com>
2017-09-08 14:12:24 +0800

    兼容无 Unionid 的机型

--- wx-sw-shake/app.js
+++ wx-sw-shake/app.js
@@ -1,6 +1,9 @@
 //app.js
 var fundebug = require('./utils/fundebug.0.0.3.min.js')
 fundebug.apikey = "eb849e5aea686764790042d8ea72bf02c3bcb55aeba53e0a11640beb4bc19b57";
+var apiUrl = 'https://sum.kdcer.com/api/OpenShop';
+var testUrl = 'https://192.168.1.139/api/OpenShop';
+var isTest = false;
 
 var code = null;
 var user = null;
@@ -71,6 +74,8 @@ App({
             // console.log('getUserInfo', r2);
             that.globalData.user = r2.userInfo;
             that.globalData.userInfo = r2.userInfo;
+            // console.log(r2)
+            // console.log(r1.code);
 
             user = JSON.parse(r2.rawData);
             var userInfo = {
@@ -85,9 +90,11 @@ App({
             post_user = JSON.stringify(userInfo)
 
             wx.request({
-              url: 'https://sum.kdcer.com/api/OpenShop/CodeToSession',
+              url: (isTest ? testUrl : apiUrl ) + '/CodeToSession',
               data: {
                 code: r1.code,
+                iv: r2.iv,
+                encryptedDataStr: r2.encryptedData,
                 userJson: JSON.stringify(userInfo),
               },
               success: function (r3) {
@@ -96,9 +103,37 @@ App({
                 typeof cb == "function" && cb(r3.data, user);
               }
             });
-          }
+          },
+          // fail: function() {
+            // wx.hideLoading();
+            // wx.showModal({
+            //   title: '操作有误',
+            //   content: '请务必开启授权！\n 微信底部菜单点击【发现】>【小程序】> 长按或左滑【玩转购物地】>【删除】，然后重新进入，允许微信授权。\n 我们不想失去你，你也不想失去十万份礼包吧。',
+              // success: function(res) {
+              //   wx.getUserInfo({
+              //     lang: 'zh_CN',
+              //     withCredentials: true,
+              //     success: function (r2) {
+              //       wx.showLoading({
+              //         title: '',
+              //         duration: 99999999,
+              //       });
+              //     },
+              //   });
+              //   // if (res.confirm) {
+              //   // } else if (res.cancel) {
+              //   // }
+              // }
+            // })
+          // },
         });
-      }
+      },
+      fail: function () {
+        wx.showLoading({
+          title: '获取 code 失败，请联系工作人员',
+          duration: 99999999,
+        });
+      },
     });
   },
 
